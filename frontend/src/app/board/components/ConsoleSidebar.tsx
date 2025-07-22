c15e5b25fd05857a1d545666608ae2d3b46e9054
eddydavies <eddy@fortick.io>
2025-07-23 00:16:20 +0100

    refactor: remove duplicate import of Image in ConsoleSidebar component
    - Clean up the ConsoleSidebar.tsx file by removing the duplicate import statement for Image from 'next/image'.

--- frontend/src/app/board/components/ConsoleSidebar.tsx
+++ frontend/src/app/board/components/ConsoleSidebar.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image';
 import { Button } from '../../../components/ui/button';
 import { useApplicants } from '../../../lib/contexts/ApplicantContext';
 import Link from 'next/link';
-import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
 interface ConsoleSidebarProps {
